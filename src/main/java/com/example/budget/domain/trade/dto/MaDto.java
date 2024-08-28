00e5acfd2702da146465849bb36cd7b59545c5ec
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-08-28 21:05:50 +0900

    feat: Add MaDto

---
+++ src/main/java/com/example/budget/domain/trade/dto/MaDto.java
@@ -0,0 +1,14 @@
+package com.example.budget.domain.trade.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.ta4j.core.num.Num;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class MaDto {
+    private Num value;
+    private Num slope;
+}
