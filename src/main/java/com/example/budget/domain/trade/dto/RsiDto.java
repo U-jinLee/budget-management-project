a2894216884353cf3621f2ce9be5c54f31c0f1c2
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-09-02 23:05:15 +0900

    feat: Add RsiDto

---
+++ src/main/java/com/example/budget/domain/trade/dto/RsiDto.java
@@ -0,0 +1,15 @@
+package com.example.budget.domain.trade.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.ta4j.core.num.Num;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class RsiDto {
+    private Num value;
+    private Num max;
+    private Num min;
+}
