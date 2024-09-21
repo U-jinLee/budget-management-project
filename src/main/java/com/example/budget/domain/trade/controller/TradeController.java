74128069a7c97ca223891d42bbe661130cd8c7ca
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-09-21 19:00:47 +0900

    feat: Create trade front page

--- src/main/java/com/example/budget/domain/trade/controller/TradeController.java
+++ src/main/java/com/example/budget/domain/trade/controller/TradeController.java
@@ -1,13 +1,25 @@
 package com.example.budget.domain.trade.controller;
 
+import com.example.budget.domain.trade.service.BybitAccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@RequiredArgsConstructor
 @Controller
 public class TradeController {
 
+    private final BybitAccountService bybitAccountService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        BigDecimal balance = bybitAccountService.getUSDTAvailableBalance().getBalance()
+                .setScale(4, RoundingMode.HALF_UP);
+        model.addAttribute("balance", balance);
         return "index";
     }
 }
